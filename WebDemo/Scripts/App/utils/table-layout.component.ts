fe0a5ada276c985e88d9d333e20e7cb4bcbc3f70
Noam <noam@fireflymigration.com>
2017-09-11 19:17:09 +0300

    first draft working

--- WebDemo/Scripts/App/utils/table-layout.component.ts
+++ WebDemo/Scripts/App/utils/table-layout.component.ts
@@ -1,8 +1,18 @@
 ﻿// https://medium.com/@ct7/building-a-reusable-table-layout-for-your-angular-2-project-adf6bba3b498
-import { Component } from '@angular/core';
+import { Component, Input, OnChanges } from '@angular/core';
 
 @Component({
     selector: 'ct-table',
     templateUrl: './scripts/app/utils/table-layout.component.html'
 })
-export class TableLayoutComponent { }
+export class TableLayoutComponent implements OnChanges {
+
+
+    @Input() records: any[];
+    @Input() caption: string;
+    keys: string[]=[];
+    ngOnChanges(): void {
+        if (this.records.length > 0)
+            this.keys = Object.keys(this.records[0]);
+    }
+}

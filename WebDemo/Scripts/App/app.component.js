0060ebc01221856ae7fdeff2e3dc111479a669e6
Noam <noam@fireflymigration.com>
2017-09-24 19:19:29 +0300

    Added angular routes

--- WebDemo/Scripts/App/app.component.js
+++ WebDemo/Scripts/App/app.component.js
@@ -8,42 +8,20 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
 Object.defineProperty(exports, "__esModule", { value: true });
 const core_1 = require("@angular/core");
 const core_2 = require("@angular/core");
-const RestList_1 = require("./utils/RestList");
-const table_layout_component_1 = require("./utils/table-layout.component");
 let AppComponent = class AppComponent {
-    constructor() {
-        this.products = new RestList_1.RestList(apiUrl + 'products');
-        this.category = new RestList_1.Lookup(apiUrl + 'categories', (product, o) => o.isEqualTo = { id: +product.categoryID });
-        this.tableSettings = new table_layout_component_1.TableSettings({
-            // /categories?_responseType=DCF
-            editable: true,
-            columnSettings: [
-                { key: "id", caption: "ProductID" },
-                { key: "productName", caption: "ProductName" },
-                { key: "supplierID", caption: "SupplierID" },
-                { key: "categoryID", caption: "CategoryID" },
-                {
-                    caption: "categoryName",
-                    getValue: (r) => this.category.get(r).categoryName,
-                    columnClass: r => this.category.found(r) ? '' : 'danger'
-                },
-                {
-                    caption: 'test', getValue: r => r.categoryID
-                }
-            ]
-        });
-    }
-    ngOnInit() {
-        this.products.get({ limit: 9995 });
-    }
 };
 AppComponent = __decorate([
     core_1.Component({
         selector: 'my-app',
-        templateUrl: `./scripts/app/app.component.html`,
+        template: `
+<nav>
+<a routerLink="/products" routerLinkActive="active">Products</a>
+<a routerLink="/categories" routerLinkActive="active">Categories</a>
+</nav>
+<router-outlet></router-outlet>
+`,
     }),
     core_2.Injectable()
 ], AppComponent);
 exports.AppComponent = AppComponent;
-const apiUrl = '/dataApi/';
 //# sourceMappingURL=app.component.js.map
---
+++ WebDemo/Scripts/App/demo/categories.ts
@@ -0,0 +1,46 @@
+﻿import { Component, OnInit } from '@angular/core';
+import { Injectable } from '@angular/core';
+import { Category } from '../models';
+import { RestList, getOptions, Lookup } from '../utils/RestList';
+import { TableSettings } from "../utils/table-layout.component";
+
+
+@Component({
+    selector: 'categories',
+    template:`
+<h1>Categories</h1>
+<ct-table [records]="categories"
+          [settings]="tableSettings"></ct-table>`
+
+})
+
+@Injectable()
+export class Categories implements OnInit {
+
+
+    categories = new RestList<Category>(apiUrl + 'categories');
+
+
+    tableSettings = new TableSettings<Category>({
+        editable: true,
+            // /categories?_responseType=DCF
+
+            columnSettings:[
+                { key: "id", caption: "CategoryID" },
+                { key: "categoryName", caption: "CategoryName" },
+                { key: "description", caption: "Description" }
+            ]
+    });
+
+
+    ngOnInit(): void {
+        this.categories.get({ limit: 5 });
+    }
+
+
+}
+const apiUrl = '/dataApi/';
+
+
+
+

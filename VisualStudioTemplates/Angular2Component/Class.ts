338ff6b77f2cb4a5c07a758dccc160feb0289fb1
Noam <noam@fireflymigration.com>
2017-09-25 15:19:28 +0300

    Created visual studio templates to make life easier

---
+++ VisualStudioTemplates/Angular2Component/Class.ts
@@ -0,0 +1,19 @@
+﻿import { Component, OnInit, Injectable } from '@angular/core';
+import { TableSettings } from "./utils/table-layout.component";
+
+
+@Component({
+    selector: '$safeitemrootname$',
+    $safeitemrootname$:`
+<h1>Categories</h1>
+<ct-table [settings]="settings"></ct-table>
+`
+})
+
+@Injectable()
+export class $safeitemrootname$  {
+
+   
+
+}
+const apiUrl = '/dataApi/';
---
+++ VisualStudioTemplates/ViewModelHelper/Class.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Firefly.Box;
+using ENV;
+using ENV.Web;
+
+namespace $rootnamespace$
+{
+	class $safeitemrootname$ : ViewModelHelper
+	{
+        public $safeitemrootname$()
+        {
+        }
+	}
+}

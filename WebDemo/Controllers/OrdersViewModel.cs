89e6230a0c6a57292e34e40c00d4974ae5fd23f4
Noam <noam@fireflymigration.com>
2017-09-26 20:56:16 +0300

    added the ViewModel

---
+++ WebDemo/Controllers/OrdersViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Firefly.Box;
+using ENV;
+using ENV.Web;
+
+namespace WebDemo.Controllers
+{
+    class OrdersViewModel : ViewModelHelper
+    {
+        public readonly Northwind.Models.Orders Orders = new Northwind.Models.Orders();
+
+        public OrdersViewModel()
+        {
+            From = Orders;
+            AllowUpdate = true;
+        }
+    }
+}

96347061f6df3f8c7d943dcef2e1c4728b3993f2
Noam <noam@fireflymigration.com>
2017-09-26 18:19:41 +0300

    Added orders

---
+++ WebDemo/Scripts/App/orders.ts
@@ -0,0 +1,18 @@
+﻿import { Component, OnInit, Injectable } from '@angular/core';
+import { TableSettings } from "./utils/table-layout.component";
+
+
+@Component({
+    template:`
+<h1>Orders</h1>
+<ct-table [settings]="settings"></ct-table>
+`
+})
+
+@Injectable()
+export class orders  {
+
+   
+
+}
+const apiUrl = '/dataApi/';

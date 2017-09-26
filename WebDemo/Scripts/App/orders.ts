dd5b74742a647a5b1f4ef40d817ecd75e5280c76
Noam <noam@fireflymigration.com>
2017-09-26 22:46:28 +0300

    Added the order type to improve intelisense

--- WebDemo/Scripts/App/orders.ts
+++ WebDemo/Scripts/App/orders.ts
@@ -1,21 +1,28 @@
 ﻿import { Component, OnInit, Injectable } from '@angular/core';
 import { TableSettings } from "./utils/table-layout.component";
+import { order } from './models';
 
 
 @Component({
-    template:`
+    template: `
 <h1>Orders</h1>
 <ct-table [settings]="settings"></ct-table>
 `
 })
 
 @Injectable()
-export class orders  {
+export class orders {
 
-    settings = new TableSettings({
+    settings = new TableSettings<order>({
         restUrl: apiUrl + 'orders'
-        , columnKeys: ["id", "customerID","companyName", "orderDate", "shipVia"]
-        , editable:true
+        , columnKeys: ["id", "customerID", "companyName", "orderDate", "shipVia"]
+        , editable: true
+        , get: {
+            sort: "orderDate",
+            order: "desc",
+            isEqualTo: { shipVia: 1, customerID:"ALFKI" }
+        }
+
     })
 
 }

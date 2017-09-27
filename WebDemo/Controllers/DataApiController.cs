f86a98405ddff3d4e7566815e196e9cd1925b767
Noam Honig <noam@fireflymigration.com>
2017-09-27 18:07:38 +0300

    making the data api updatable

--- WebDemo/Controllers/DataApiController.cs
+++ WebDemo/Controllers/DataApiController.cs
@@ -13,7 +13,7 @@ namespace WebDemo.Controllers
         static DataApiController()
         {
             _dataApi.Register(typeof(Northwind.Models.Categories));
-            _dataApi.Register(typeof(Northwind.Models.Orders));
+            _dataApi.Register(typeof(Northwind.Models.Orders),true);
 
         }
         // GET: DataApi

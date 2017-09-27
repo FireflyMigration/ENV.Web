c4ccd2aa0c64055b218918f65f1785fe1c6e6148
Noam <noam@fireflymigration.com>
2017-09-27 12:52:43 +0300

    #16

--- VisualStudioTemplates/Angular2Component/Class.ts
+++ VisualStudioTemplates/Angular2Component/Class.ts
@@ -1,12 +1,10 @@
 ﻿import { Component, OnInit, Injectable } from '@angular/core';
+import * as utils from './lib/utils';
 import * as models from './models';
-import { TableSettings } from "./utils/table-layout.component";
-import { Lookup, RestList } from './utils/RestList';
 
 @Component({
     template:`
 <h1>$safeitemrootname$</h1>
-<ct-table [settings]="settings"></ct-table>
 `
 })
 
--- WebDemo/Scripts/App/lib/utils.ts
+++ WebDemo/Scripts/App/lib/utils.ts
@@ -1,19 +1,19 @@
 ﻿
 import { Component, Input, OnChanges } from '@angular/core';
-import { RestList, getOptions, Lookup } from './RestList';
+
 
 
 @Component({
-    selector: 'ct-table',
-    templateUrl: './scripts/app/utils/table-layout.component.html'
+    selector: 'data-grid',
+    templateUrl: './scripts/app/lib/data-grid.component.html'
 })
 
-export class TableLayoutComponent implements OnChanges {
+export class DataGridComponent implements OnChanges {
 
 // Inspired by  https://medium.com/@ct7/building-a-reusable-table-layout-for-your-angular-2-project-adf6bba3b498
 
     @Input() records: Iterable<any>;
-    @Input() settings = new TableSettings();
+    @Input() settings = new DataSettings();
     columnMaps: ColumnSettingBase[];
     rowButtons: rowButtonBase[] = [];
     keys: string[] = [];
@@ -177,7 +177,7 @@ export class TableLayoutComponent implements OnChanges {
 function makeTitle(key: string) {
     return key.slice(0, 1).toUpperCase() + key.replace(/_/g, ' ').slice(1);
 }
-class TableSettingsBase {
+class DataSettingsBase {
     editable = false;
     settings: ColumnSettingBase[] = [];
     buttons: rowButtonBase[] = [];
@@ -185,12 +185,12 @@ class TableSettingsBase {
     rowClass?: (row: any) => string;
     onSavingRow?: (s: ModelState<any>) => void;
 }
-export class TableSettings<rowType> extends TableSettingsBase {
+export class DataSettings<rowType> extends DataSettingsBase {
     static getRecords(): any {
         throw new Error("Method not implemented.");
     }
 
-    constructor(settings?: TableSettingsInterface<rowType>) {
+    constructor(settings?: IDataSettings<rowType>) {
         super();
         if (settings) {
             if (settings.columnKeys)
@@ -267,7 +267,7 @@ export class TableSettings<rowType> extends TableSettingsBase {
     }
 
 }
-interface TableSettingsInterface<rowType> {
+interface IDataSettings<rowType> {
     editable?: boolean,
     columnSettings?: ColumnSetting<rowType>[],
     columnKeys?: string[],
@@ -332,4 +332,199 @@ interface rowButton<rowType> extends rowButtonBase {
 function isFunction(functionToCheck) {
     var getType = {};
     return functionToCheck && getType.toString.call(functionToCheck) === '[object Function]';
+}
+
+
+export class RestList<T extends hasId> implements Iterable<T>{
+    [Symbol.iterator](): Iterator<T> {
+        return this.items[Symbol.iterator]();
+    }
+
+
+    items: (restListItem & T)[] = [];
+    constructor(private url: string) {
+
+    }
+    private map(item: T): restListItem & T {
+
+        let x = <any>item;
+        let id = x.id;
+        x.save = () => this.save(id, x);
+        x.delete = () => {
+            return fetch(this.url + '/' + id, { method: 'delete' }).then(onSuccess, onError).then(() => {
+                this.items.splice(this.items.indexOf(x), 1);
+            });
+
+        }
+        return <restListItem & T>x;
+    }
+
+    get(options?: getOptions<T>) {
+
+        let url = new urlBuilder(this.url);
+        if (options) {
+            url.addObject({
+                _limit: options.limit,
+                _page: options.page,
+                _sort: options.orderBy,
+                _order: options.orderByDir
+            });
+            url.addObject(options.isEqualTo);
+            url.addObject(options.isGreaterOrEqualTo, "_gte");
+            url.addObject(options.isLessOrEqualTo, "_lte");
+            url.addObject(options.isGreaterThan, "_gt");
+            url.addObject(options.isLessThan, "_lt");
+            url.addObject(options.isDifferentFrom, "_ne");
+        }
+
+
+        return myFetch(url.url).then(r => {
+            let x: T[] = r;
+            this.items = r.map(x => this.map(x));
+        });
+    }
+    add(): T {
+        let x: newItemInList = { newRow: true };
+        this.items.push(this.map(x as any as T));
+        return x as any as T;
+    }
+    private save(id: any, c: restListItem & T) {
+
+        let nr: newItemInList = c as any as newItemInList;
+        if (nr.newRow)
+            return myFetch(this.url, {
+                method: 'post',
+                headers: {
+                    'Content-type': "application/json"
+                },
+                body: JSON.stringify(c)
+            }).then(response => {
+
+                this.items[this.items.indexOf(c)] = this.map(response);
+            });
+        else {
+
+            return myFetch(this.url + '/' + id, {
+                method: 'put',
+                headers: {
+                    'Content-type': "application/json"
+                },
+                body: JSON.stringify(c)
+            }).then(response => {
+
+                this.items[this.items.indexOf(c)] = this.map(response);
+            });
+        }
+    }
+
+}
+class urlBuilder {
+    constructor(public url: string) {
+    }
+    add(key: string, value: any) {
+        if (value == undefined)
+            return;
+        if (this.url.indexOf('?') >= 0)
+            this.url += '&';
+        else
+            this.url += '?';
+        this.url += encodeURIComponent(key) + '=' + encodeURIComponent(value);
+    }
+    addObject(object: any, suffix = '') {
+        if (object != undefined)
+            for (var key in object) {
+                this.add(key + suffix, object[key]);
+            }
+    }
+}
+function myFetch(url: string, init?: RequestInit): Promise<any> {
+
+    return fetch(url, init).then(onSuccess, error => {
+
+    });
+
+}
+function onSuccess(response: Response) {
+
+    if (response.status >= 200 && response.status < 300)
+        return response.json();
+    else throw response;
+
+}
+function onError(error: any) {
+    throw error;
+}
+interface newItemInList {
+    newRow: boolean;
+}
+interface hasId {
+    id?: any;
+}
+interface restListItem {
+    save: () => void;
+    delete: () => void;
+}
+export interface getOptions<T> {
+    isEqualTo?: T;
+    isGreaterOrEqualTo?: T;
+    isLessOrEqualTo?: T;
+    orderBy?: string;
+    orderByDir?: string;
+    page?: number;
+    limit?: number;
+    isGreaterThan?: T;
+    isLessThan?: T;
+    isDifferentFrom?: T;
+
+}
+
+export class Lookup<lookupType, idType_or_MainTableType> {
+
+    constructor(url: string, private options?: (mt: idType_or_MainTableType, o: getOptions<lookupType>) => void) {
+        if (!options) {
+            this.options = (mt, o) => o.isEqualTo = <lookupType><any>{ id: mt };
+        }
+        this.categories = new RestList<lookupType>(url);
+    }
+
+    categories: RestList<lookupType>;
+    private cache: {};
+
+    get(r: any): lookupType {
+        return this.getInternal(r).value;
+    }
+    found(r: any): boolean {
+        return this.getInternal(r).found;
+    }
+
+    private getInternal(r: any): lookupRowInfo<lookupType> {
+
+        let find: getOptions<lookupType> = {};
+        this.options(<idType_or_MainTableType>r, find);
+        let key = JSON.stringify(find);
+        if (this.cache == undefined)
+            this.cache = {};
+        if (this.cache[key]) {
+            return this.cache[key];
+        } else {
+            let res = new lookupRowInfo<lookupType>();
+            this.cache[key] = res;
+            this.categories.get(find).then(() => {
+                res.loading = false;
+                if (this.categories.items.length > 0) {
+                    res.value = this.categories.items[0];
+                    res.found = true;
+                }
+            });
+            return res;
+        }
+
+    }
+}
+
+class lookupRowInfo<type> {
+    found = false;
+    loading = true;
+    value: type = {} as type;
+
 }

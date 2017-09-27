c759948c15b0df7c35b13602ccd1a742d261bde1
Noam Honig <noam@fireflymigration.com>
2017-09-27 18:37:24 +0300

    adding a page to our angualr application

---
+++ WebDemo/Scripts/App/Orders.ts
@@ -0,0 +1,17 @@
+﻿import { Component, OnInit, Injectable } from '@angular/core';
+import * as utils from './lib/utils';
+import * as models from './models';
+
+@Component({
+    template:`
+<h1>Orders </h1>
+`
+})
+
+@Injectable()
+export class Orders  {
+
+   
+
+}
+const apiUrl = '/dataApi/';

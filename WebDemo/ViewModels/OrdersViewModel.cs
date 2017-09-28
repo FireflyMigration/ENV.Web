038f1787ded2bfeb16b253754a2b01b227753667
Noam Honig <noam@fireflymigration.com>
2017-09-28 08:04:56 +0300

    Server side relation

--- WebDemo/ViewModels/OrdersViewModel.cs
+++ WebDemo/ViewModels/OrdersViewModel.cs
@@ -10,17 +10,22 @@ namespace WebDemo.ViewModels
     class OrdersViewModel : ViewModelHelper
     {
         public readonly Northwind.Models.Orders Orders = new Northwind.Models.Orders();
+        public readonly Northwind.Models.Shippers Shippers = new Northwind.Models.Shippers();
 
         public OrdersViewModel()
         {
             From = Orders;
+
+            Relations.Add(Shippers, Shippers.ShipperID.IsEqualTo(Orders.ShipVia));
+
             AllowUpdate = true;
             Where.Add(Orders.OrderDate.IsGreaterOrEqualTo(1997, 1, 1));
-
+            Shippers.CompanyName.Caption = "Shipper Name";
             MapColumn(Orders.OrderID,
                 Orders.CustomerID,
                 Orders.OrderDate,
-                Orders.ShipVia);
+                Orders.ShipVia,
+                Shippers.CompanyName);
             MapExperssion("ServerSideDayOfWeek", () => u.NDOW(u.DOW(Orders.OrderDate)));
         }
     }

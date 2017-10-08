5c957b23d15449edb80505d9ffec0cf78ba5b5b3
Noam Honig <noam@fireflymigration.com>
2017-10-08 09:03:37 +0300

    Separate the html from the typescript

--- WebDemo/Scripts/App/Orders.ts
+++ WebDemo/Scripts/App/Orders.ts
@@ -3,19 +3,7 @@ import * as utils from './lib/utils';
 import * as models from './models';
 
 @Component({
-    template: `
-<h1>{{ title }} ({{title.length}})</h1>
-<input [(ngModel)]="title">
-<h2 *ngIf="title.length>15"> the title is long!!!</h2>
-<button (click)="click()">my button</button>
-<br/>
-<ul>
-<li *ngFor="let o of orders">
-Order: {{o.orderId}}<br/>
-Customer: {{o.customerId}}
-</li>
-</ul>
-`
+    templateUrl:'/scripts/app/orders.html'
 })
 
 @Injectable()

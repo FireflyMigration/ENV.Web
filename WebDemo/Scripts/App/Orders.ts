994e15f937174fb2bea58280f4c13896283be470
Noam Honig <noam@fireflymigration.com>
2017-10-13 08:18:19 +0300

    Added the orders component.

---
+++ WebDemo/Scripts/App/Orders.ts
@@ -0,0 +1,12 @@
+﻿import { Component } from '@angular/core';
+import * as utils from './lib/utils';
+import * as models from './models';
+
+@Component({
+    template:`
+<h1>Orders</h1>
+`
+})
+export class Orders  {
+
+}

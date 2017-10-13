80bd6ea5bc12e565f49205ac68812502761c607e
Noam Honig <noam@fireflymigration.com>
2017-10-13 08:08:21 +0300

    adding the apis

--- WebDemo/Controllers/DataApiController.cs
+++ WebDemo/Controllers/DataApiController.cs
@@ -13,6 +13,11 @@ namespace WebDemo.Controllers
         static DataApiController()
         {
             _dataApi.Register(typeof(Northwind.Models.Categories),true);
+            _dataApi.Register(typeof(Northwind.Models.Orders));
+            _dataApi.Register(typeof(Northwind.Models.Order_Details));
+            _dataApi.Register(typeof(Northwind.Models.Customers));
+            _dataApi.Register(typeof(Northwind.Models.Shippers));
+            _dataApi.Register(typeof(Northwind.Models.Products));
         }
         // GET: DataApi
         public void Index(string name, string id = null)

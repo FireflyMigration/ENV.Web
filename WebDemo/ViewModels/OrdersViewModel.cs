d00f6c83b309bb1250ec23436a59d2c29d2bd436
Noam Honig <noam@fireflymigration.com>
2017-10-15 08:08:34 +0300

    Added Orders View Model

---
+++ WebDemo/ViewModels/OrdersViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Firefly.Box;
+using ENV;
+using ENV.Web;
+
+namespace WebDemo.ViewModels
+{
+    class OrdersViewModel : ViewModel
+    {
+        public OrdersViewModel()
+        {
+        }
+    }
+}

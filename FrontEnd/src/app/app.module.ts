fe3808ca6b6df49be3306e2bfc68fde7dc630ed0
Noam Honig <noam@fireflymigration.com>
2017-11-03 09:45:58 +0200

    Add radWebModule to app module

--- FrontEnd/src/app/app.module.ts
+++ FrontEnd/src/app/app.module.ts
@@ -2,13 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AlertModule,TabsModule } from "ngx-bootstrap";
 import { AppComponent } from './app.component';
+import { radWebModule} from 'radweb';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule,TabsModule.forRoot()
+    BrowserModule,TabsModule.forRoot(),radWebModule
   ],
   providers: [],
   bootstrap: [AppComponent]

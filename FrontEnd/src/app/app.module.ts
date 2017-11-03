b976534d8eb9c297e06e17f2af8ff380a1403edf
Noam Honig <noam@fireflymigration.com>
2017-11-03 09:27:41 +0200

    Installed bootstrap and jquery, and added it to the cli.json and app.module
    npm install bootstrap jquery ngx-bootstrap --save
    npm install @types/jquery --save-dev

--- FrontEnd/src/app/app.module.ts
+++ FrontEnd/src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { AlertModule,TabsModule } from "ngx-bootstrap";
 import { AppComponent } from './app.component';
 
 @NgModule({
@@ -8,7 +8,7 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,TabsModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]

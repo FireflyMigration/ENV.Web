f95b3134967088fefe220474a1141c55ad5907c7
Noam Honig <noam@fireflymigration.com>
2017-11-03 08:58:46 +0200

    added the modules from ngx to the AppModule

--- frontEnd/src/app/app.module.ts
+++ frontEnd/src/app/app.module.ts
@@ -1,14 +1,15 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { AlertModule,TabsModule } from "ngx-bootstrap";
 import { AppComponent } from './app.component';
+
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,AlertModule.forRoot(),TabsModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
